7fd798d5c713f96a9452ec419f78fc140b4f99d1
Christian Jones <elmoooo@gmail.com>
2015-04-29 20:04:41 +0100

    Don't hide self when vanishing

--- src/main/java/com/doctordark/base/user/BaseUser.java
+++ src/main/java/com/doctordark/base/user/BaseUser.java
@@ -15,7 +15,12 @@ import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.UUID;
 
 public class BaseUser implements ConfigurationSerializable {
 
@@ -215,7 +220,8 @@ public class BaseUser implements ConfigurationSerializable {
         player.spigot().setCollidesWithEntities(!vanished);
         VanishPriority playerPriority = VanishPriority.of(player);
         for (Player target : Bukkit.getServer().getOnlinePlayers()) {
-            if ((vanished) && (playerPriority.isMoreThan(VanishPriority.of(target)))) {
+            if (player.equals(target)) continue;
+            if (vanished && playerPriority.isMoreThan(VanishPriority.of(target))) {
                 target.hidePlayer(player);
             } else {
                 target.showPlayer(player);
