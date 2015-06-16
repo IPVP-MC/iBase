55af9d1212df2b92795793b2b97f4f473f62c70f
DoctorDark <doctordark11@gmail.com>
2015-06-16 13:10:03 +0100

    Allow using /skull to spawn WITHER, CREEPER, etc. typed skulls

--- src/main/java/com/doctordark/base/command/module/inventory/SkullCommand.java
+++ src/main/java/com/doctordark/base/command/module/inventory/SkullCommand.java
@@ -1,8 +1,14 @@
 package com.doctordark.base.command.module.inventory;
 
 import com.doctordark.base.command.BaseCommand;
+import com.google.common.base.Enums;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.SkullType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -11,11 +17,23 @@ import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.DoubleStream;
 
 /**
  * Command used to spawn skulls of players.
  */
 public class SkullCommand extends BaseCommand {
+
+    private static final ImmutableList<String> SKULL_NAMES;
+
+    static {
+        ImmutableList.Builder<String> builder = new ImmutableList.Builder<>();
+        for (SkullType skullType : SkullType.values()) {
+            builder.add(skullType.name());
+        }
+
+        SKULL_NAMES = builder.build();
+    }
 
     public SkullCommand() {
         super("skull", "Spawns a player head skull item.", "base.command.skull");
@@ -35,21 +53,37 @@ public class SkullCommand extends BaseCommand {
             return true;
         }
 
-        Player player = (Player) sender;
-        String ownerName = (args.length > 0) ? args[0] : sender.getName();
+        Optional<SkullType> skullType = args.length > 0 ? Enums.getIfPresent(SkullType.class, args[0]) : Optional.absent();
+        ItemStack stack;
+        if (skullType.isPresent()) {
+            stack = new ItemStack(Material.SKULL_ITEM, 1, skullType.get().getData());
+        } else {
+            stack = new ItemStack(Material.SKULL_ITEM, 1, SkullType.PLAYER.getData());
+            String ownerName = (args.length > 0) ? args[0] : sender.getName();
+            SkullMeta meta = (SkullMeta) stack.getItemMeta();
+            meta.setOwner(ownerName);
+            stack.setItemMeta(meta);
+        }
 
-        ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
-        SkullMeta meta = (SkullMeta) skull.getItemMeta();
-        meta.setOwner(ownerName);
-        meta.setDisplayName(ChatColor.GREEN + ownerName);
-        skull.setItemMeta(meta);
-
-        player.getInventory().addItem(skull);
+        ((Player) sender).getInventory().addItem(stack);
         return true;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-        return (args.length == 1) ? null : Collections.<String>emptyList();
+        if (args.length != 1) {
+            return Collections.emptyList();
+        }
+
+        List<String> completions = Lists.newArrayList(SKULL_NAMES);
+        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+            if (sender instanceof Player && !((Player) sender).canSee(player)) {
+                continue;
+            }
+
+            completions.add(player.getName());
+        }
+
+        return getCompletions(args, completions);
     }
 }
