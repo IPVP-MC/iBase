1af405b9779053e7a3dfeacc95e5c21ed4fa9449
DoctorDark <doctordark11@gmail.com>
2015-09-13 01:27:15 +0100

    Show time the server started with /uptime

--- src/main/java/com/doctordark/base/command/module/essential/UptimeCommand.java
+++ src/main/java/com/doctordark/base/command/module/essential/UptimeCommand.java
@@ -2,13 +2,18 @@ package com.doctordark.base.command.module.essential;
 
 import com.doctordark.base.command.BaseCommand;
 import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.apache.commons.lang3.time.FastDateFormat;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import java.lang.management.ManagementFactory;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class UptimeCommand extends BaseCommand {
+
+    private static final FastDateFormat TIME_FORMATTER = FastDateFormat.getInstance("dd/MM HH:mm:ss", TimeZone.getTimeZone("GMT+1"), Locale.ENGLISH);
 
     public UptimeCommand() {
         super("uptime", "Check the uptime of the server.");
@@ -17,8 +22,9 @@ public class UptimeCommand extends BaseCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        String upTime = DurationFormatUtils.formatDurationWords(System.currentTimeMillis() - ManagementFactory.getRuntimeMXBean().getStartTime(), true, true);
-        sender.sendMessage(ChatColor.GREEN + "Server up-time: " + ChatColor.LIGHT_PURPLE + upTime + ChatColor.GREEN + '.');
+        long startTime = ManagementFactory.getRuntimeMXBean().getStartTime();
+        String upTime = DurationFormatUtils.formatDurationWords(System.currentTimeMillis() - startTime, true, true);
+        sender.sendMessage(ChatColor.BLUE + "Server up-time: " + ChatColor.GOLD + upTime + ChatColor.BLUE + ", started at " + TIME_FORMATTER.format(startTime) + ".");
         return true;
     }
 }
